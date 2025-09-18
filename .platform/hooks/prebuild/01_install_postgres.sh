267fe5ea85c03385dda9a4af42acbc777186bfd0
dhanusuya-348 <dhanurk25@gmail.com>
2025-09-18 23:26:14 +0530

    EB environments created successfully.

---
+++ .platform/hooks/prebuild/01_install_postgres.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yum install -y postgresql-devel gcc gcc-c++ python3-devel
--- application.py
+++ application.py
@@ -57,5 +57,7 @@ def get_watchlist(user_sub):
     session.close()
     return jsonify(out)
 
+application = app
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
